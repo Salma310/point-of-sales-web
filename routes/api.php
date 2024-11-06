27749345e1dc0b0b667902f9d660324aba6781c9
Salma310 <nasywatwentytwo@gmail.com>
2024-11-06 14:13:43 +0700

    Add Restful API - User CRUD

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\LevelController;
+use App\Http\Controllers\Api\UserController;
 use Monolog\Level;
 
 /*
@@ -17,17 +18,17 @@ use Monolog\Level;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
 Route::get('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
 // Route::middleware('auth:sanctum')->get('/user', [LoginController::class, 'getUser'])->name('user');
-Route::middleware('auth:api')->get('/user', function (Request $request){
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request){
+//     return $request->user();
+// });
 Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
 
 Route::get('levels', [LevelController::class, 'index']);
@@ -35,3 +36,9 @@ Route::post('levels', [LevelController::class, 'store']);
 Route::get('levels/{level}', [LevelController::class, 'show']);
 Route::put('levels/{level}', [LevelController::class, 'update']);
 Route::delete('levels/{level}', [LevelController::class, 'destroy']);
+
+Route::get('users', [App\Http\Controllers\Api\UserController::class, 'index']);
+Route::post('users', [App\Http\Controllers\Api\UserController::class, 'store']);
+Route::get('users/{user}', [App\Http\Controllers\Api\UserController::class, 'show']);
+Route::put('users/{user}', [App\Http\Controllers\Api\UserController::class, 'update']);
+Route::delete('users/{user}', [App\Http\Controllers\Api\UserController::class, 'destroy']);
